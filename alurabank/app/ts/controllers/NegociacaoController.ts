3de7199da0da244ef3a4c57269b9081be8cc04cf
irt_paulo <luis.oliveira@reconcavo.org.br>
2019-06-17 13:44:02 -0300

    Criação de modelo para encapsular coleção de Negociações, permitindo apenas a adição de elementos à lista e impedindo qualquer outra operação de alteração ou remoção. Aplicação de programação defensiva para que a operação de acesso ao array retorne apenas uma cópia, protegendo a referência de qualquer modificação. Por fim, a especificação do tipo de retorno para auxiliar o autocomplete e o controle do compilador sobre o tipo retornado.

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes  = new Negociacoes();
 
     constructor() {
         this._inputData = <HTMLInputElement> document.querySelector('#data');
@@ -20,6 +21,7 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        console.log(this._negociacoes);
     }
 }
