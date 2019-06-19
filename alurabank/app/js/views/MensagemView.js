0e1c0bea695a19e7eda5316104eded0f3088bda3
irt_paulo <luis.oliveira@reconcavo.org.br>
2019-06-19 14:33:55 -0300

    Configurando classe View para lidar com um tipo genérico em seus métodos, tornando possível suas classes filhas a trabalharem com tipos específicos. Enquanto MensagemView agora trabalha com string, NegociacoesView trabalha do o modelo de Negociacoes.

--- alurabank/app/js/views/MensagemView.js
+++ alurabank/app/js/views/MensagemView.js
@@ -1,7 +1,4 @@
 class MensagemView extends View {
-    update(modelo) {
-        this._elemento.innerHTML = this.template(modelo);
-    }
     template(modelo) {
         return `<p class="alert alert-info">${modelo}</p>`;
     }
